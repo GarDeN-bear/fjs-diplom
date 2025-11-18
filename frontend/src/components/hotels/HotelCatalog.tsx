5daaa0d27292c92243e4c6f246e2da765f3f9b38
Денис Гарпинич <d.garpinich@wisetech.pro>
2025-11-18 19:42:51 +0300

    Добавлена пагинация

--- frontend/src/components/hotels/HotelCatalog.tsx
+++ frontend/src/components/hotels/HotelCatalog.tsx
@@ -5,22 +5,52 @@ import RoomCard from "./hotel-rooms/RoomCard";
 import { Link } from "react-router-dom";
 
 const HotelCatalog = () => {
-  const [hotels, setHotels] = useState<utils.Hotel[]>([]);
+  const limit = 1000;
+  const itemsInPage = 10;
+  const [hotels, setAllHotels] = useState<utils.Hotel[]>([]);
+  const [hotelsOnPage, setHotelsOnPage] = useState<utils.Hotel[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageNumbers, setPageNumbers] = useState<number[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchHotels().finally(() => setLoading(false));
+    fetchAllHotels().finally(() => setLoading(false));
   }, []);
 
-  const fetchHotels = async () => {
+  useEffect(() => {
+    fetchHotelsOnPage();
+  }, [hotels, currentPage]);
+
+  const fetchHotelsOnPage = async () => {
+    if (hotels.length <= 0) {
+      return;
+    }
+
+    const firstIndex = (currentPage - 1) * itemsInPage;
+    const page = hotels.slice(firstIndex, firstIndex + itemsInPage);
+    console.log("page", page)
+    setHotelsOnPage(page);
+  };
+
+  const fetchAllHotels = async () => {
     try {
       const response = await fetch(
         `${
           utils.VITE_BACKEND_URL
-        }/api/common/hotels?limit=${utils.limit.toString()}&offset=${utils.offset.toString()}`
+        }/api/common/hotels?limit=${limit.toString()}&offset=${(
+          (currentPage - 1) *
+          itemsInPage
+        ).toString()}`
       );
       const data: utils.Hotel[] = await response.json();
-      setHotels(data);
+      setAllHotels(data);
+      setPageNumbers([]);
+      const totalPages = Math.ceil(data.length / itemsInPage);
+      const numbers = [];
+      for (let i = 1; i <= totalPages; i++) {
+        numbers.push(i);
+      }
+      setPageNumbers(numbers);
     } catch (error) {
       console.log("Ошибка: ", error);
     }
@@ -33,7 +63,7 @@ const HotelCatalog = () => {
         <div>Загрузка...</div>
       ) : (
         <div className="hotels-list">
-          {hotels.map((hotel) => (
+          {hotelsOnPage.map((hotel) => (
             <div key={hotel._id} className="hotel-card">
               <RoomCard hotelId={hotel._id} />
               <div className="hotel-card-description">
@@ -47,6 +77,14 @@ const HotelCatalog = () => {
           ))}
         </div>
       )}
+      <div className="pagination">
+        {pageNumbers.length > 1 &&
+          pageNumbers.map((num) => (
+            <button key={num} className={`pagination-btn ${currentPage==num ? "active" : ""}`} onClick={() => setCurrentPage(num)}>
+              {num}
+            </button>
+          ))}
+      </div>
     </section>
   );
 };
--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -19,12 +19,12 @@ export interface CreateHotelForm {
 export interface CreateHotelRoomForm {
   hotel: string;
   description?: string;
-  images?: FileList | null;
+  images?: FileList|null;
   isEnabled?: boolean;
 }
 
-export const limit: number = 10;
+export const limit: number = 2;
 export const offset: number = 0;
 
 export const VITE_BACKEND_URL =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+    import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
